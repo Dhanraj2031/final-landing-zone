afa04572226558f8efc72b267792d20f8e172ce3
Vaibhav M U <Vaibhav.U@sonata-software.com>
2025-02-28 13:03:21 +0530

    test code apgw

---
+++ modules/hub-sp-rt/variable.tf
@@ -0,0 +1,33 @@
+variable "rt_table_name" {
+    type = string
+  
+}
+variable "rg_location" {
+  type = string
+}
+variable "rg_name" {
+  type = string
+}
+variable "route_name1" {
+  type = string
+}
+variable "next_hop_type" {
+  type = string
+}
+variable "next_hop_in_ip_address" {
+  type = string
+}
+variable "route_name2" {
+    type = string
+  
+}
+variable "spoke2_address_space" {
+  type = string
+}
+variable "spoke1_address_space" {
+  type = string
+}
+variable "subnet_id" {
+    type = string
+  
+}
